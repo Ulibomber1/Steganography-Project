c6adb2555332b99b30b8cd50f3b8ac4291ea795d
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2018-03-19 07:57:42 -0700

    GUI 0.2
    Next Version

--- 1.3.3.py
+++ 1.3.3.py
@@ -3,58 +3,32 @@ import Tkinter
 import binascii
 import numpy as np
 
-
-
-
 ##########
-#Functions
+#Tkinter GUI
 ##########
 
-def rgb2hex(r, g, b):
-    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+#Root Window
+root = Tkinter.Tk()
+root.wm_title('Steganography Hider/Retreiver')
 
-def hex2rgb(hexcode):
-    
-    return tuple(map(ord, hexcode[1:].decode('hex'))) 
+#Model
+filename = Tkinter.StringVar()
+filename.set('')
+message = Tkinter.StringVar()
+message.set('')
 
-#Returns hexadecimal value of the user's string.
-def str2bin(message):
-    print 'str2bin before:'
-    print message
-    print 'str2bin step1:'
-    print binascii.hexlify(message)
-    print 'str2bin step2:'
-    print int(binascii.hexlify(message), 16)
-    binary = bin(int(binascii.hexlify(message), 16))
-    print 'str2bin Final:'
-    print binary 
-    return binary[2:]
 
-def bin2str(binary):
-    print 'bin2str Before:'
-    print binary
-    print 'bin2str step1:'
-    print int('0b'+binary,2)
-    message = binascii.unhexlify('%x' % (int('0b'+binary,2)))
-    print 'bin2str Final:'
-    print message
-    return message
+#View
+file_entry = Tkinter.Entry(root, textvariable=filename)
+file_entry.grid(row=2, column=0, columnspan=2)
+message_entry = Tkinter.Text(root)
+message_entry.grid(row=1,column=0)
 
-def encode(hexcode, digit):
-	if hexcode[-1] in ('0','1', '2', '3', '4', '5'):
-		hexcode = hexcode[:-1] + digit
-		return hexcode
-	else:
-		return None
 
-def decode(hexcode):
-	if hexcode[-1] in ('0', '1'):
-		return hexcode[-1]
-	else:
-		return None
+#Controller
 
-def hide(filename, message):  
-        message = message_entry.get(0)        
+#Message Hider
+def hide(filename, message = message_entry.get(root,1)):          
 	img = Image.open(filename)
 	binary = str2bin(message) + '1111111111111110'
 	if img.mode in ('RGBA'):
@@ -104,9 +78,7 @@ def hide(filename, message):
 			
 	return "Incorrect Image Mode, Couldn't Hide"
 
-						
-				
-
+#Message Retrieval						
 def retr(filename):
 	img = Image.open(filename)
 	binary = ''
@@ -127,33 +99,61 @@ def retr(filename):
 
 		return bin2str(binary)
 	return "Incorrect Image Mode, Couldn't Retrieve"
-	
-##########
-#Tkinter GUI
-##########
-
-#Root Window
-root = Tkinter.Tk()
-root.wm_title('Steganography Hider/Retreiver')
-
-#Model
-filename = Tkinter.StringVar()
-filename.set('')
-message = Tkinter.StringVar()
-message.set('')
 
 
-#View
-file_entry = Tkinter.Entry(root, textvariable=filename)
-file_entry.grid(row=0,column=0)
-message_entry = Tkinter.Text(root)
-
-
-#Controller
 hide_button = Tkinter.Button(root, command=hide,text='Hide')
 hide_button.grid(row=0, column=1)
 retr_button = Tkinter.Button(root, command=retr,text='Retr')
 retr_button.grid(row=1, column=1)
+
+
+##########
+#Functions
+##########
+
+def rgb2hex(r, g, b):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+def hex2rgb(hexcode):
+    
+    return tuple(map(ord, hexcode[1:].decode('hex'))) 
+
+#Returns hexadecimal value of the user's string.
+def str2bin(message):
+    print 'str2bin before:'
+    print message
+    print 'str2bin step1:'
+    print binascii.hexlify(message)
+    print 'str2bin step2:'
+    print int(binascii.hexlify(message), 16)
+    binary = bin(int(binascii.hexlify(message), 16))
+    print 'str2bin Final:'
+    print binary 
+    return binary[2:]
+
+def bin2str(binary):
+    print 'bin2str Before:'
+    print binary
+    print 'bin2str step1:'
+    print int('0b'+binary,2)
+    message = binascii.unhexlify('%x' % (int('0b'+binary,2)))
+    print 'bin2str Final:'
+    print message
+    return message
+
+def encode(hexcode, digit):
+	if hexcode[-1] in ('0','1', '2', '3', '4', '5'):
+		hexcode = hexcode[:-1] + digit
+		return hexcode
+	else:
+		return None
+
+def decode(hexcode):
+	if hexcode[-1] in ('0', '1'):
+		return hexcode[-1]
+	else:
+		return None
+
 
 #Main Loop
 root.mainloop()
